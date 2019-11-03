9f68617cf0782d03331a7a72031cd51f533a56d3
US31079 <US31079@HZC-VM-D2-283.accretivehealth.local>
2019-11-03 16:39:07 +0530

    TC_391267,391268,391270

--- src/test/java/r1/prcmbe/pages/DefectOverridePage.java
+++ src/test/java/r1/prcmbe/pages/DefectOverridePage.java
@@ -6,9 +6,15 @@ import java.util.List;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import r1.commons.utilities.CommonMethods;
 
 public class DefectOverridePage extends PageObject {
-
+	
+	CommonMethods commonMethods;
+	
+	private final String defaultValueDefectTypeDrpDwn = "Uncategorized Defect";
+	private final String defaultValueDefectSubCatTypeDrpDwn = "Select Sub-Defect Category";
+	
 	@FindBy(xpath = "//*[@id='divDefectWorkflow']/h3[contains(text(),'Defect Workflow')]")
 	private WebElementFacade defectWorkflowSecHeader;
 
@@ -20,7 +26,55 @@ public class DefectOverridePage extends PageObject {
 
 	@FindBy(xpath = "//h2[contains(text(),'Current Defect')]")
 	private WebElementFacade defectSubCategrySec;
-
+	
+	@FindBy(xpath = "//*[@id='rdoAssignCat']//td/label")
+	private List<WebElementFacade> listOfRadioBtnOnOverrideSubCat;
+	
+	@FindBy(xpath = "//*[@id='ddlDefType']")
+	private WebElementFacade defectTypeValuesDrpdwn;
+	
+	@FindBy(xpath = "//*[@id='ddlSubCate']")
+	private WebElementFacade defectSubCategoryValuesDrpdwn;
+	
+	@FindBy(xpath = "//*[@id='btnResetSave']")
+	private WebElementFacade saveButton;
+	
+	@FindBy(id = "lblCurrentDefectSubCategory")
+	private WebElementFacade defectSubCatUnderCurrentDefect;
+	
+	@FindBy(xpath = "//p[@class='dc-text']/strong[contains(text(),'Defect Classification')]")
+	private WebElementFacade defectClassificationSection;
+	
+	@FindBy(xpath = "//*[@id='lblBreadcrumb']")
+	private WebElementFacade bredCrumUnderDefectClassification;
+	
+	@FindBy(xpath = "//a[@class='btn btnPrimary next action-button']")
+	private WebElementFacade nextButton;
+	
+	@FindBy(xpath = "//*[@id='verify']/h2")
+	private WebElementFacade triageSectionHeader;
+	
+	@FindBy(xpath = "//a[starts-with(@id,'btnVerify')]")
+	private List<WebElementFacade> listOfBtnOnDefectWorkFlow;
+	
+	@FindBy(id = "btnVerifyPreviousStep")
+	private WebElementFacade prevButton;
+	
+	@FindBy(id = "overrideStep")
+	private WebElementFacade defectOverrideSec;
+	
+	@FindBy(xpath = "//*[@id='cblActionsOptional']/label")
+	private List<WebElementFacade> listOfSOPActionsOnTriagePage;
+	
+	@FindBy(xpath = "//*[@id='cblActionsRequired']/label")
+	private List<WebElementFacade> listOfSOPActionsOnActionPage;
+	
+	@FindBy(xpath = "//*[@id='required']/h2")
+	private WebElementFacade actionSectionHeader;
+	
+	@FindBy(id = "btnVerifyNextStep")
+	private WebElementFacade nextBtnOnTriagePage;
+	
 	public boolean isDefectWorkFlowSecPresent() {
 		return defectWorkflowSecHeader.isVisible();
 	}
@@ -39,10 +93,129 @@ public class DefectOverridePage extends PageObject {
 
 	public boolean isAssignedSubCategryVisible() {
 		return defectSubCategrySec.isVisible();
-
 	}
 
 	public boolean verifyDefaultSelectedStepOnProgressBar(String stepName) {
 		return confirmStepOnProgressBar.getText().equals(stepName);
 	}
+
+	public void selectRadioBtnOnOverrideSubCat(String radioBtnValue) {
+		int size = listOfRadioBtnOnOverrideSubCat.size();
+		for (int i = 1; i < size; i++) {
+			if(listOfRadioBtnOnOverrideSubCat.get(i).getText().equals(radioBtnValue)) {
+				listOfRadioBtnOnOverrideSubCat.get(i).click();
+			}
+		}
+	}
+	
+  	public List<String> getDefectTypeValues() {
+	List<String> listOfDefectTypeValue;
+	listOfDefectTypeValue = defectTypeValuesDrpdwn.getSelectOptions();
+	return listOfDefectTypeValue;
+}
+  	
+	public String selectAndGetTextDefectType() {
+		List<String> listOfDefectTypeDropDwnValues = getDefectTypeValues();
+		int randomNumber;
+		int size = listOfDefectTypeDropDwnValues.size();
+		randomNumber = CommonMethods.getRandom(size);
+		defectTypeValuesDrpdwn.selectByIndex(randomNumber);
+
+		while (defectTypeValuesDrpdwn.getSelectedVisibleTextValue().equals(defaultValueDefectTypeDrpDwn)) {
+			randomNumber = CommonMethods.getRandom(size);
+			defectTypeValuesDrpdwn.selectByIndex(randomNumber);
+		}
+
+		
+		return defectTypeValuesDrpdwn.getSelectedVisibleTextValue();
+}
+
+  	public List<String> getDefectSubCategoryValues() {
+	List<String> listOfDefectSubCategory;
+	listOfDefectSubCategory = defectSubCategoryValuesDrpdwn.getSelectOptions();
+	return listOfDefectSubCategory;
+}
+	 
+	
+	public String selectAndGetTextDefectSubCategory() {
+		List<String> listOfDefectSubCatDropDwnValues = getDefectSubCategoryValues();
+		int randomNumber;
+		int size = listOfDefectSubCatDropDwnValues.size();
+		randomNumber = CommonMethods.getRandom(size);
+		defectSubCategoryValuesDrpdwn.selectByIndex(randomNumber);
+		
+		while (defectSubCategoryValuesDrpdwn.getSelectedVisibleTextValue().equals(defaultValueDefectSubCatTypeDrpDwn)) {
+			randomNumber = CommonMethods.getRandom(size);
+			defectSubCategoryValuesDrpdwn.selectByIndex(randomNumber);
+		}
+		return defectSubCategoryValuesDrpdwn.getSelectedVisibleTextValue();
+	}
+
+	public void clickOnSaveBtn() {
+		saveButton.click();
+	}
+
+	public void refreshesAPage() {
+		 getDriver().navigate().refresh();
+	}
+
+	public String getDefectSubCategoryUnderCurrentDefect() {
+		return defectSubCatUnderCurrentDefect.getText();
+	}
+
+	public void moveToDefectClassificationSec() {
+		withAction().moveToElement(defectClassificationSection).build().perform();
+	}
+
+	public String getDefectCategoryUnderDefectClassSec() {
+		return bredCrumUnderDefectClassification.getText();
+	}
+
+	public void clickOnNextButton() {
+		nextButton.click();
+	}
+
+	public String getTriageSectionHeaderText() {
+		return triageSectionHeader.getText();		
+	}
+
+	public List<String> getButtonValuesOnSection() {
+		List<String> listOfBtnValues = new ArrayList<>();
+		for (WebElementFacade ele : listOfBtnOnDefectWorkFlow) {
+			listOfBtnValues.add(ele.getText());
+		}
+		return listOfBtnValues;
+	}
+
+	public void clickOnPrevBtn() {
+		prevButton.click();
+	}
+
+	public boolean checkOverrideDefectCatSec() {
+		return defectOverrideSec.isVisible();
+	}
+
+	public List<String> getSOPActionsOnTriagePage() {
+		List<String> listOfSOPActions = new ArrayList<>();
+		for (WebElementFacade ele : listOfSOPActionsOnTriagePage) {
+			listOfSOPActions.add(ele.getText());
+		}
+		return listOfSOPActions;
+	}
+
+	public String getActionSectionHeaderText() {
+       return actionSectionHeader.getText();
+	}
+
+	public List<String> getSOPActionsOnActionPage() {
+		List<String> listOfSOPActions = new ArrayList<>();
+		for (WebElementFacade ele : listOfSOPActionsOnActionPage) {
+			listOfSOPActions.add(ele.getText());
+		}
+		return listOfSOPActions;
+	}
+
+	public void clickOnNextButtonOnTriagePage() {
+		nextBtnOnTriagePage.click();
+	}	   
 }
---
+++ src/test/java/r1/prcmbe/serenity/steps/DefectOverrideSteps.java
@@ -0,0 +1,20 @@
+package r1.prcmbe.serenity.steps;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.thucydides.core.annotations.Step;
+import r1.commons.databaseconnection.DatabaseConn;
+
+public class DefectOverrideSteps {
+
+	@Step
+	public boolean verifySelectedDefectSubCategoryWithDB(String selectedSubCatValue) throws SQLException {
+		List<String> listOfDefectSubCategoryInDB = new ArrayList<>();
+		while (DatabaseConn.resultSet.next()) {
+			listOfDefectSubCategoryInDB.add(DatabaseConn.resultSet.getString("DefectSubCategoryDesc"));
+		}
+		return listOfDefectSubCategoryInDB.contains(selectedSubCatValue);
+	}
+}
