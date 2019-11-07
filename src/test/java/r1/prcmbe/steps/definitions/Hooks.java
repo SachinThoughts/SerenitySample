96967f625771483b80195f60ffbf81783bfc6936
Vijayshree Chaturvedi <vchaturvedi@r1rcm.com>
2019-11-07 07:36:47 +0000

    Hooks.java edited online with Bitbucket

--- src/test/java/r1/prcmbe/steps/definitions/Hooks.java
+++ src/test/java/r1/prcmbe/steps/definitions/Hooks.java
@@ -19,7 +19,7 @@ public class Hooks extends PageObject {
 	@Steps
 	LoginSteps loginStep;
 
-	@Before(value = "@434762 or @434763 or @434764 or @434765 or @434766")
+	@Before(value = "@AHtoDecisionAdmin or @PRCMUser")
 	public void prcmBeUser() throws IOException {
 		open();
 		if (accInfoPage.checkLogoutVisible() && propertyName != "prcmBeUsername") {
