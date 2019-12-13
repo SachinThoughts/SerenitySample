3611ffa4eed22395844b5bb529f6444350c3fc38
US31083 <US31083@HZC-VM-D2-288.accretivehealth.local>
2019-12-13 07:51:05 +0530

    TC 391051 implemented

--- src/test/java/r1/prcmbe/pages/AccountActionHistoryPage.java
+++ src/test/java/r1/prcmbe/pages/AccountActionHistoryPage.java
@@ -7,69 +7,50 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 
-public class AccountActionHistoryPage extends PageObject{
+public class AccountActionHistoryPage extends PageObject {
 
-	@FindBy(xpath="//ul[@id='notesHistory']//li[1]//span/span[2]")
+	@FindBy(xpath = "//ul[@id='notesHistory']//li[1]//span/span[2]")
 	private List<WebElementFacade> listOfRecentAddedAccountActionHistoryVal;
-	
-	@FindBy(xpath="//ul[@id='notesHistory']//li[1]//span/span[1]")
+
+	@FindBy(xpath = "//ul[@id='notesHistory']//li[1]//span/span[1]")
 	private List<WebElementFacade> listOfRecentAddedAccountActionHistoryLabel;
+
+	@FindBy(xpath = "//button[@id='btnShowHistory']/span[text()='Show Account Action History Notes']")
+	private WebElementFacade showAccHistoryBtn;
 	
-	@FindBy(xpath="//*[@id='accountactionhistory']//a[@class='flex-prev']")
-	private WebElementFacade prevArrow;
+	@FindBy(id="accountactionhistory")
+	private WebElementFacade accActionHistorySection;
 	
-	@FindBy(xpath="//*[@id='accountactionhistory']//a[@class='flex-next']")
-	private WebElementFacade nextArrow;
-	
-	@FindBy(xpath="//*[@class='process-type-history']//span")
-	private WebElementFacade defectSubCategoryType;
-	
-	@FindBy(xpath = "//*[@id='carousel']//li[last()]/div[last()]//a[@class='trigger']/..")
-	private WebElementFacade addedBubble;
-	
-	@FindBy(xpath = "//div[@class='popover-content']//span//span[1]")
-	private List<WebElementFacade> listOfEventCircleColumns;
-	
-	public List<String> getListOfRecentAddedAccountActionHistoryLabel(){
-		List<String> listOfRecentAddedAccntLabel=new ArrayList<>();
-		for (WebElementFacade recentAddedAccntLabel:listOfRecentAddedAccountActionHistoryLabel) {
+	@FindBy(id="lblNoHistory")
+	private WebElementFacade noAccActionHistoryMsgLbl;
+
+	public List<String> getListOfRecentAddedAccountActionHistoryLabel() {
+		List<String> listOfRecentAddedAccntLabel = new ArrayList<>();
+		for (WebElementFacade recentAddedAccntLabel : listOfRecentAddedAccountActionHistoryLabel) {
 			listOfRecentAddedAccntLabel.add(recentAddedAccntLabel.getText());
 		}
 		return listOfRecentAddedAccntLabel;
 	}
-	
+
 	public String getRecentAddedAccountActionHistoryValue(int positionOfElement) {
 		return listOfRecentAddedAccountActionHistoryVal.get(positionOfElement).getText();
 	}
-	
+
 	public boolean isRecentAddedAccountActionHistoryLabelVisible(int labelPosition) {
 		return listOfRecentAddedAccountActionHistoryLabel.get(labelPosition).isVisible();
 	}
-	
-	public void clickPreviousArrow() {
-		if(prevArrow.isClickable()) {
-			prevArrow.click();
+
+	public void ifVisibleClickShowAccHistoryBtn() {
+		if (showAccHistoryBtn.isVisible()) {
+			evaluateJavascript("arguments[0].click();",showAccHistoryBtn);
 		}
 	}
 	
-	public boolean isPreviousDefectSubCategoryVisible() {
-			return defectSubCategoryType.isVisible();
+	public void scrollToAccActionHistorySection() {
+		withAction().moveToElement(accActionHistorySection).build().perform();
 	}
 	
-	public String getHandoffBubbleColor() {
-		withAction().moveToElement(addedBubble).build().perform();
-		return addedBubble.getCssValue("background-color");
-	}
-	
-	public void hoverOnAddedBubble() {
-		withAction().moveToElement(addedBubble).build().perform();
-	}
-	
-	public List<String> getListOfEventCircleColumns() {
-		List<String> listOfEventCircleCols = new ArrayList<>();
-		for (WebElementFacade eventCircleCol : listOfEventCircleColumns) {
-			listOfEventCircleCols.add(eventCircleCol.getText().trim());
-		}
-		return listOfEventCircleCols;
+	public String getNoAccActionHistoryMsg() {
+		return noAccActionHistoryMsgLbl.getText();
 	}
 }
--- src/test/java/r1/prcmbe/steps/definitions/AccountActionHistoryStepDef.java
+++ src/test/java/r1/prcmbe/steps/definitions/AccountActionHistoryStepDef.java
@@ -1,91 +1,45 @@
 package r1.prcmbe.steps.definitions;
 
 import java.sql.SQLException;
-import java.util.List;
 
 import org.junit.Assert;
 
-import cucumber.api.DataTable;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import r1.commons.databaseconnection.DatabaseConn;
 import r1.commons.utilities.CommonMethods;
 import r1.prcmbe.pages.AccountActionHistoryPage;
-import r1.prcmbe.pages.AccountInformationPage;
-import r1.prcmbe.pages.DefectWorkflowPage;
 import r1.prcmbe.pages.SearchPage;
 
 public class AccountActionHistoryStepDef {
-	AccountInformationPage accInfoPage;
 	AccountActionHistoryPage accActionHistoryPage;
 	CommonMethods commonMethods;
-	SearchPage searchPage;
-	DefectWorkflowPage defectWorkflowPage;
 	String invoiceNumber;
+	SearchPage searchPage;
 	static String dbFileName = "AccountActionHistory";
-	private final String BUBBLECOLOR = "rgba(61, 100, 168, 1)";
+		
+	@When("^user scrolls down to reach Account Action History Section$")
+	public void user_scrolls_down_to_reach_Account_Action_History_Section() {
+		accActionHistoryPage.scrollToAccActionHistorySection();
+	}
 
-	@Given("^user is on Account Information Page$")
-	public void user_is_on_Account_Information_Page() {
-		accInfoPage.verifyPatientDetailsSectionVisible();
+	@Then("^user should be able to view the message \"([^\"]*)\" at Account Action History Section$")
+	public void user_should_be_able_to_view_the_message_at_Account_Action_History_Section(String noActionHistoryMsg) {
+		Assert.assertTrue("No Account Action history message is not displayed", accActionHistoryPage.getNoAccActionHistoryMsg().equals(noActionHistoryMsg));
 	}
 	
 	@When("^user runs the account action history query \"([^\"]*)\"$")
 	public void user_runs_the_account_action_history_query(String queryName) throws ClassNotFoundException, SQLException, Exception {
 		DatabaseConn.serverConn(DatabaseConn.serverName, DatabaseConn.databaseName,
 				commonMethods.loadQuery(queryName, dbFileName));
-		System.out.println(queryName);
+		
+			while (DatabaseConn.resultSet.next()) {
+				invoiceNumber=DatabaseConn.resultSet.getString("InvoiceNumber");
+			}
 	}
 	
-	@When("^user fetch invoice number from database$")
-	public void user_fetch_invoice_number_from_database() throws SQLException {
-		while (DatabaseConn.resultSet.next()) {
-			invoiceNumber = DatabaseConn.resultSet.getString("InvoiceNumber");
-		}
-		System.out.println(invoiceNumber);
-	}
-	
-	@When("^user enters invoice number fetched from database$")
-	public void user_enters_invoice_number_fetched_from_database() {
+	@When("^user enters the result in Invoice Number search textbox$")
+	public void user_enters_the_result_in_Invoice_Number_search_textbox() {
 		searchPage.enterInvoiceNumber(invoiceNumber);
-	}
-
-	@When("^user clicks on back and forth arrows$")
-	public void user_clicks_on_back_and_forth_arrows() {
-		accActionHistoryPage.clickPreviousArrow();
-	}
-
-	@Then("^user should be able to view all defect subcategory previously associated with that invoice$")
-	public void user_should_be_able_to_view_all_defect_subcategory_previously_associated_with_that_invoice() {
-	    Assert.assertTrue("Defect subcategory is not visible", accActionHistoryPage.isPreviousDefectSubCategoryVisible());
-	}
-
-	@When("^user selects any checkbox in Verify All Steps Taken Section$")
-	public void user_selects_any_checkbox_in_Verify_All_Steps_Taken_Section() {
-		defectWorkflowPage.selectRandomVerifyAllStepsCheckbox();
-	}
-
-	@When("^user selects any checkbox in steps Taken Section$")
-	public void user_selects_any_checkbox_in_steps_Taken_Section() {
-		defectWorkflowPage.selectRandomStepsTakenCheckbox();
-	}
-
-	@Then("^user should be able to view the Blue bubble code display as D on horizontal timeline$")
-	public void user_should_be_able_to_view_the_Blue_bubble_code_display_as_D_on_horizontal_timeline() {
-		Assert.assertTrue("User is not able to view blue bubble with D text",
-				accActionHistoryPage.getHandoffBubbleColor().equals(BUBBLECOLOR));
-	}
-
-	@When("^user hovers the activity bubbles$")
-	public void user_hovers_the_activity_bubbles() {
-		accActionHistoryPage.hoverOnAddedBubble();
-	}
-
-	@Then("^user should be able to view all fields of that action$")
-	public void user_should_be_able_to_view_all_fields_of_that_action(DataTable expectedFields) {
-		List<String> expectedListOfEventCircleCols = expectedFields.asList(String.class);
-		Assert.assertTrue("The column headers in the popup text are not as expected.",
-				accActionHistoryPage.getListOfEventCircleColumns().equals(expectedListOfEventCircleCols));
 	}
 }
