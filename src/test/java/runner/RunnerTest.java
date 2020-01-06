cd239f7e6e64527dfde4ed852366c009517a0d46
Rushikesh Deshmukh <rdesmukh@r1rcm.com>
2020-01-06 11:18:13 +0530

    Revert "grid related changes"
    This reverts commit bd1983f0b186352fbe29bd1f847e9ee29191e31b.

---
+++ src/test/java/runner/RunnerTest.java
@@ -0,0 +1,15 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+
+@CucumberOptions(features = { "src/test/resources/features/PRCM-Backend" }, tags = {
+
+		"@ProfessionalUDC or @ExternalSearch or @DefectOverride or @DefaultHandoff or @FinancialInformation or @WorkflowConfiguration or @InternalSearch or @Tagging or @WorkflowDistribution or @CallPayerQueue or @AccountDocument or @BSODayNight or @PhysicianNPIConfiguration or @TaggingHistory or @EparsProHandoff or @FacilityGroupConfiguration or @PRCMSave" }, glue = {
+				"r1.prcmbe.steps.definitions" })
+public class RunnerTest {
+
+}
